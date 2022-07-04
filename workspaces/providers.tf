6112499cb01fd319a7ebaee69a4551f80198a315
Lev Andelman <levandelman@Cheburashka.lan>
2022-07-04 08:08:02 +0300

    troubleshooting with terraform console

--- workspaces/providers.tf
+++ workspaces/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+    backend "remote" {
+    organization = "TeraSky"
+      workspaces {
+        name = "wordpress-aws-workspaces"
+      }
+    }
   required_providers {
     tfe = {
       version = "~> 0.30.2"
