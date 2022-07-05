d7d6ee11850bb551a670232c7747ce03aaada1da
Lev Andelman <levandelman@Cheburashka.local>
2022-07-05 06:32:07 +0300

    dbinstance name can't contain dash

--- rds/rds.tf
+++ rds/rds.tf
@@ -9,7 +9,7 @@ module "db" {
   instance_class    = "db.t4g.micro"
   allocated_storage = 5
 
-  db_name  = "${var.name}-wordpress"
+  db_name  = "${var.name}db"
   username = "user"
   port     = "3306"
 
