55fbfeb0d687297b499ec2aa78d1b080ff5afeda
Lev Andelman <levandelman@Cheburashka.local>
2022-07-06 15:24:03 -0400

    adding route53 route for autovalidation

--- wordpress-compute/wordpress.tf
+++ wordpress-compute/wordpress.tf
@@ -175,6 +175,23 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
+resource "aws_route53_record" "example" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.lev_labs.zone_id
+}
+
 resource "aws_route53_record" "wordpress" {
   zone_id = data.aws_route53_zone.lev_labs.zone_id
   name    = "${var.name}.lev-labs.com"
