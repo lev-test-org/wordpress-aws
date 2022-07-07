ec6be81b63bf16ccebd567e5ca922f750f3b49ce
Lev Andelman <levandelman@Cheburashka.local>
2022-07-07 18:34:12 -0400

    fixing db_init.sh

--- db_initializer/db_init.sh
+++ db_initializer/db_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /var/www/html
+cd /tmp
 /usr/local/bin/wp core download
-/usr/local/bin/wp config create --path=. --dbname=$DBNAME --dbuser=$DBUSER --dbpass=$DBPASS --dbhost=$DBHOST --skip-check
-/usr/local/bin/wp core install --url="https://$DOMAIN" --title="$DOMAIN" --admin_user=$DBUSER --admin_password=$DBPASS --dbhost=$DBHOST --skip-email
+/usr/local/bin/wp config create --path=. --dbname="${DBNAME}" --dbuser="${DBUSER}" --dbpass="${DBPASS}" --dbhost="${DBHOST}" --skip-check
+/usr/local/bin/wp core install --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --dbhost="${DBHOST}" --skip-email
