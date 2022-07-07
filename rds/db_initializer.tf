b2c2aab75def6507b0c47d1ce49f1069d4160e48
Lev Andelman <levandelman@Cheburashka.local>
2022-07-07 15:06:15 -0400

    adding role to lambda

--- rds/db_initializer.tf
+++ rds/db_initializer.tf
@@ -2,6 +2,7 @@ resource aws_lambda_function db_initalizer {
  function_name = "${var.name}-lambda"
  image_uri = "public.ecr.aws/v4n1y6t4/db_initializer:v1"
  package_type = "Image"
+ role          = aws_iam_role.iam_for_lambda.arn
  environment {
     variables = {
       DOMAIN = "${var.name}.lev-labs.com"
@@ -15,4 +16,24 @@ resource aws_lambda_function db_initalizer {
     subnet_ids         = [data.terraform_remote_state.vpc.outputs.vpc.private_subnets]
     security_group_ids = [data.terraform_remote_state.vpc.outputs.db_initializer_sg.id]
   }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
