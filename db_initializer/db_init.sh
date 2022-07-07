b291b554f23fd4e016c2ca3298f8d334c73a209a
Lev Andelman <levandelman@Cheburashka.local>
2022-07-07 19:02:43 -0400

    adding email

--- db_initializer/db_init.sh
+++ db_initializer/db_init.sh
@@ -9,4 +9,4 @@ echo "running /usr/local/bin/wp core download"
 echo "running /usr/local/bin/wp config create --path=. --dbname=${DBNAMEg} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check"
 /usr/local/bin/wp config create --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
 echo "running /usr/local/bin/wp core install --url=https://${DOMAIN} --title=${DOMAIN} --admin_user=${DBUSER} --admin_password=${DBPASS} --dbhost=${DBHOST} --skip-email"
-/usr/local/bin/wp core install --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --dbhost="${DBHOST}" --skip-email
+/usr/local/bin/wp core install --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --dbhost="${DBHOST}" --admin_email="admin@${DOMAIN}" --skip-email
