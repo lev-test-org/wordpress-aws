fc97fcd77e99ec73e05112c5e7c1b233cf09e206
Lev Andelman <levandelman@Cheburashka.local>
2022-07-07 18:40:38 -0400

    troubleshooting db_init.sh

--- db_initializer/db_init.sh
+++ db_initializer/db_init.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo "variables"
+env
+echo "changing dir"
 cd /tmp
+echo "$PWD"
+echo "running /usr/local/bin/wp core download"
 /usr/local/bin/wp core download
-/usr/local/bin/wp config create --path=. --dbname="${DBNAME}" --dbuser="${DBUSER}" --dbpass="${DBPASS}" --dbhost="${DBHOST}" --skip-check
+echo "running /usr/local/bin/wp config create --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check"
+/usr/local/bin/wp config create --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
+echo "running /usr/local/bin/wp core install --url=https://${DOMAIN} --title=${DOMAIN} --admin_user=${DBUSER} --admin_password=${DBPASS} --dbhost=${DBHOST} --skip-email"
 /usr/local/bin/wp core install --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --dbhost="${DBHOST}" --skip-email
