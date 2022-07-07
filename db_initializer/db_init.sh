78abf6894216c3764f938d0168ccdf91eeeb9201
Lev Andelman <levandelman@Cheburashka.local>
2022-07-07 19:19:40 -0400

    adding debug

--- db_initializer/db_init.sh
+++ db_initializer/db_init.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-echo "variables"
-env
 echo "changing dir"
 cd /tmp
 echo "$PWD"
 echo "running /usr/local/bin/wp core download"
 /usr/local/bin/wp core download
-echo "running /usr/local/bin/wp config create --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check"
-/usr/local/bin/wp config create --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
-echo "running /usr/local/bin/wp core install --url=https://${DOMAIN} --title=${DOMAIN} --admin_user=${DBUSER} --admin_password=${DBPASS} --dbhost=${DBHOST} --skip-email"
+/usr/local/bin/wp config create --debug --force --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
 /usr/local/bin/wp core install --debug --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --admin_email="admin@${DOMAIN}" --skip-email
