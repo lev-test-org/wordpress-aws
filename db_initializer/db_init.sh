502046f20f26d733f04ae1f16a579d96024acdf8
Lev Andelman <levandelman@Cheburashka.local>
2022-07-08 14:54:11 -0400

    fixing bash respone in lambda

--- db_initializer/db_init.sh
+++ db_initializer/db_init.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "changing dir"
 cd /tmp
-echo "$PWD"
 HEADERS="$(mktemp /tmp/)"
 EVENT_DATA=$(curl -sS -LD "$HEADERS" -X GET "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next")
+echo "event data $EVENT_DATA"
 REQUEST_ID=$(grep -Fi Lambda-Runtime-Aws-Request-Id "$HEADERS" | tr -d '[:space:]' | cut -d: -f2)
+echo "request id $REQUEST_ID"
 echo "running /usr/local/bin/wp core download"
 /usr/local/bin/wp core download
-/usr/local/bin/wp config create --debug --force --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
-/usr/local/bin/wp core install --debug --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --admin_email="admin@${DOMAIN}" --skip-email
+/usr/local/bin/wp config create  --force --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
+/usr/local/bin/wp core install --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --admin_email="admin@${DOMAIN}" --skip-email
 if [ $? == 0 ]; then
+  echo "running curl -X POST \"http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response\"  -d \"SUCCESS\""
   curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response"  -d "SUCCESS"
 else
   curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/init/error"  -d "FAIL"
