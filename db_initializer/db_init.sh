9cd385a3bf6b4c02d75c6fc9659f03567a2f2832
Lev Andelman <levandelman@Cheburashka.local>
2022-07-08 15:02:26 -0400

    fixing bash respone in lambda

--- db_initializer/db_init.sh
+++ db_initializer/db_init.sh
@@ -9,9 +9,10 @@ echo "running /usr/local/bin/wp core download"
 /usr/local/bin/wp core download
 /usr/local/bin/wp config create  --force --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
 /usr/local/bin/wp core install --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --admin_email="admin@${DOMAIN}" --skip-email
-if [ $? == 0 ]; then
+if [ $? -eq 0 ]; then
   echo "running curl -X POST \"http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response\"  -d \"SUCCESS\""
   curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response"  -d "SUCCESS"
 else
+  echo "curl -X POST \"http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/init/error\"  -d \"FAIL\""
   curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/init/error"  -d "FAIL"
 fi
