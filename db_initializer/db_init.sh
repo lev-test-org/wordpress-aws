b1df5e64e15efefd449bf60b89cbe03a8a36b689
Lev Andelman <levandelman@Cheburashka.local>
2022-07-08 14:44:32 -0400

    fixing bash respone in lambda

--- db_initializer/db_init.sh
+++ db_initializer/db_init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-HEADERS="$(mktemp)"
-EVENT_DATA=$(curl -sS -LD "$HEADERS" -X GET "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next")
-REQUEST_ID=$(grep -Fi Lambda-Runtime-Aws-Request-Id "$HEADERS" | tr -d '[:space:]' | cut -d: -f2)
 echo "changing dir"
 cd /tmp
 echo "$PWD"
+HEADERS="$(mktemp)"
+EVENT_DATA=$(curl -sS -LD "$HEADERS" -X GET "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/next")
+REQUEST_ID=$(grep -Fi Lambda-Runtime-Aws-Request-Id "$HEADERS" | tr -d '[:space:]' | cut -d: -f2)
 echo "running /usr/local/bin/wp core download"
 /usr/local/bin/wp core download
 /usr/local/bin/wp config create --debug --force --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
