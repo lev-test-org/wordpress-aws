efd960cffa91b536fc45658e9cf6cf0595b51b72
Lev Andelman <levandelman@Cheburashka.local>
2022-07-08 16:57:17 -0400

    checkig what's wrong with refresh of ctonainerush

--- db_initializer/db_init.sh
+++ db_initializer/db_init.sh
@@ -7,11 +7,11 @@ echo "event data $EVENT_DATA"
 REQUEST_ID=$(grep -Fi Lambda-Runtime-Aws-Request-Id "$HEADERS" | tr -d '[:space:]' | cut -d: -f2)
 echo "request id $REQUEST_ID"
 echo "running /usr/local/bin/wp core download"
-/usr/local/bin/wp core download
+/usr/local/bin/wp core --debug download
 echo "running wp config create"
-/usr/local/bin/wp config create  --force --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
+/usr/local/bin/wp config create --debug --force --path=. --dbname=${DBNAME} --dbuser=${DBUSER} --dbpass=${DBPASS} --dbhost=${DBHOST} --skip-check
 echo "running wp core install"
-/usr/local/bin/wp core install --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --admin_email="admin@${DOMAIN}" --skip-email
+/usr/local/bin/wp core install --debug --url="https://${DOMAIN}" --title="${DOMAIN}" --admin_user="${DBUSER}" --admin_password="${DBPASS}" --admin_email="admin@${DOMAIN}" --skip-email
 if [ $? -eq 0 ]; then
   echo "running curl -X POST \"http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response\"  -d \"SUCCESS\""
   curl -X POST "http://${AWS_LAMBDA_RUNTIME_API}/2018-06-01/runtime/invocation/$REQUEST_ID/response"  -d "SUCCESS"
