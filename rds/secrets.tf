f94ea388e5437b628c7fe7535c4e385c114f3381
Lev Andelman <levandelman@Cheburashka.lan>
2022-07-26 11:41:11 +0300

    seperating username and passwords to 2 secrets

--- rds/secrets.tf
+++ rds/secrets.tf
@@ -1,11 +1,14 @@
-resource "aws_secretsmanager_secret" "rds-creds" {
-  name = "${var.name}-rds-creds"
+resource "aws_secretsmanager_secret" "rds-username" {
+  name = "${var.name}-rds-user"
+}
+resource "aws_secretsmanager_secret" "rds-password" {
+  name = "${var.name}-rds-password"
 }
 resource "aws_secretsmanager_secret_version" "rds-username" {
-  secret_id     = aws_secretsmanager_secret.rds-creds.id
+  secret_id     = aws_secretsmanager_secret.rds-username.id
   secret_string = module.db.db_instance_username
 }
 resource "aws_secretsmanager_secret_version" "rds-password" {
-  secret_id     = aws_secretsmanager_secret.rds-creds.id
+  secret_id     = aws_secretsmanager_secret.rds-password.id
   secret_string = module.db.db_instance_password
 }
