f3eaba6aaa11421195304db96b02afb687eb1447
Lev Andelman <levandelman@Cheburashka.lan>
2022-09-30 13:55:10 +0300

    added nullable

--- vpc/active_dns.tf
+++ vpc/active_dns.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "wordpress" {
   count = var.active_dns ? 1 : 0
   zone_id = data.aws_route53_zone.lev_labs.zone_id
-  name    = "active-${var.env}-${var.name}.lev-labs.com"
+  name    = "active-${var.name}.lev-labs.com"
   type    = "CNAME"
   ttl     = "300"
   records = [var.active_dns]
